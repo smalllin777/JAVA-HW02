525dcdc8ece3d16477414bc06da2816cd7945310
smalllin777 <125981111+smalllin777@users.noreply.github.com>
2023-03-02 21:06:03 +0800

    Create A1113318_0224_01.java

---
+++ A1113318_0224_01.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class A1113318_0224_01
+{
+
+	public static void main(String[]argv)
+	{
+		int n;
+		System.out.println("請輸入整數");
+		Scanner num=new Scanner(System.in);
+		n=num.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("偶數");
+		}else{
+			System.out.println("奇數");
+		}
+	}
+}		
