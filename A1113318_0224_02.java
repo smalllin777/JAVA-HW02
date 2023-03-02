2d95448df8aaacbb4a94fe0cf25189a70834724c
smalllin777 <125981111+smalllin777@users.noreply.github.com>
2023-03-02 21:06:43 +0800

    Create A1113318_0224_02.java

---
+++ A1113318_0224_02.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class A1113318_0224_02
+{
+	public static void main(String[]argv)
+	{
+		System.out.println("請輸入攝氏溫度");
+		Scanner temp=new Scanner(System.in);
+		int n=temp.nextInt();
+		System.out.println("華氏溫度為:" +(n*9/5+32));
+	}
+}
